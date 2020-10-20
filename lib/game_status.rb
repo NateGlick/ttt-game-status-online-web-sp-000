c707403a98e7ebdb03a08e6ca0d95a76ddf9ea4a
Nathan Glick <nateleighglick@hotmail.com>
2020-10-20 08:06:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,10 +22,7 @@ def won?(board)
     win_index_3 = win_combination[2]
     
     
-    board[win_index_1] == board[win_index_2] == "X" && position_3 == "X"
-        return win_combination
-      else position_1 == "O" && position_2 =="O" && position_3 == "O"
-        return win_combination
+    board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3]
       end
         return false
      end
